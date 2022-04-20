a557321d74aac3c0b054d6b05e0a2a85d8221f90
HamingnottT <Jnottingham836@yahoo.com>
2022-04-20 11:54:12 -0500

    Hello World

---
+++ src/main/scala/playground/HelloWorld.scala
@@ -0,0 +1,11 @@
+package playground
+
+object HelloWorld {
+  
+ def main(args: Array[String]): Unit = {
+        println("\n\n")
+        println("hello world") 
+        println("\n\n")
+    }   
+}
+
