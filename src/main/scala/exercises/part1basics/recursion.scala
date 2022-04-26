3d391cbd651ef6c81f635d162c86d4be0436f805
HamingnottT <Jnottingham836@yahoo.com>
2022-04-26 15:39:03 -0500

    add lecture notes: recursion

---
+++ src/main/scala/exercises/part1basics/recursion.scala
@@ -0,0 +1,43 @@
+package exercises.part1basics
+
+import scala.annotation.tailrec
+import scala.io.StdIn.readLine
+
+// import scala.compiletime.ops.string
+
+object recursion {
+    
+    // 1) string concatenator
+        // 1. My attempt:
+    def stringConcat(str: String, n: Int): String = {
+        @tailrec
+        def concatHelper(n: Int, accumulator: String): String = 
+            if (n == 1) accumulator
+            else concatHelper(n - 1, str + accumulator)
+
+        concatHelper(n, str)
+    }
+
+        // 2. Answer key:
+    def concatenateTailrec(aString: String, n: Int, accumulator: String): String =
+        if (n <= 0) accumulator
+        else concatenateTailrec(aString, n-1, aString + accumulator)
+
+        /**Notes: my attempt came out pretty close to daniel's solution.
+         * Mine was modeled heavily after the original string concat function in lectures/functions.scala
+         * with elements of the tail recurion function just learned in lectures/Recursion.scala.
+         * 
+         * Both solutions work quite well. What can be taken away from this exercise is that the tail recursion function can be
+         * optimized further, taking the accumulator in the main function and thus no longer needing an auxillary function to help the main.
+        */
+
+    def main(args: Array[String]): Unit = {
+        var inputStr = readLine("Input string: ")
+        var inputInt = readLine("Input number: ").toInt
+        
+        println(stringConcat(inputStr, inputInt))
+        println(concatenateTailrec(inputStr, inputInt, ""))
+
+    }
+
+}
--- src/main/scala/lectures/part1basics/Recursion.scala
+++ src/main/scala/lectures/part1basics/Recursion.scala
@@ -1,30 +1,61 @@
 package lectures.part1basics
 
-import scala.annotation.tailrec
+import scala.io.StdIn.readLine
+
+import scala.annotation.tailrec       //essential import for tail recursion
+
+/** About the JVM:
+ * in order to run a recursive function, the JVM uses a call stack to keep partial results
+ * so that it can get back to computing the desired result.
+ * This call stack can be vizualized as a stack of books. Each time you add a book, you stack one on top of the other.
+ * The call stack is similar to this as it organizes calls in the aformentioned exampled.
+ * The call stack also executes in a last-in-first-out way.
+*/
 
 object Recursion extends App {
-
+  var counter = 0
+  // example of a stack frame analysis using the factorial function
   def factorial(n: Int): Int =
     if (n <= 1) 1
     else {
-      println("Computing factorial of " + n + " - I first need factorial of " + (n-1))
+      // println("Part 1 - Pre-evaluation:\n")
+      println(s"Frame ${counter + 1}: " + "Computing factorial of " + n + " - I first need factorial of " + (n-1))
+      counter += 1
       val result = n * factorial(n-1)
-      println("Computed factorial of " + n)
+      // println("Part 2 - base case reached, now evaluating:\n")
+      println(s"Frame ${counter + 1}: " + "Computed factorial of " + n)
+      counter += 1
 
       result
     }
 
-  println(factorial(10))
+  var input = 10
+  println(s"\nCall stack analysis of factorial(${input}):\n")
+  println(factorial(input))
   //  println(factorial(5000)) // stack overflow!
 
+  counter = 0
+
   def anotherFactorial(n: Int): BigInt = {
-    @tailrec
+    @tailrec  //this annotation tells the compiler to verify and treat this function as tail recursive
     def factHelper(x: Int, accumulator: BigInt): BigInt =
       if (x <= 1) accumulator
-      else factHelper(x - 1, x * accumulator) // TAIL RECURSION = use recursive call as the LAST expression
+      else {
+        println(s"test start ${counter + 1}")
+        counter += 1
+        factHelper(x - 1, x * accumulator) // TAIL RECURSION = use recursive call as the LAST expression
+      }
 
     factHelper(n, 1)
   }
+
+  /** Adding an auxillary function with an accmulator works well to prevent a stack overflow error.
+   * The accumulator's job is to record and accumulate each subsequent value on the recursive call.
+   * 
+   * The trick in this example is that the factHelper() function is the last expression of anotherFactorial's codepath.
+   * Because of this, Scala preserves the same stack frame and does not use additional stack frames for recursive calls.
+   * 
+  */
 
   /*
     Breakdown:
@@ -38,8 +69,9 @@ object Recursion extends App {
     = factHelper(1, 1 * 2 * 3 * 4 * ... * 10)
     = 1 * 2 * 3 * 4 * ... * 10
    */
-
-  println(anotherFactorial(20000))
+    println(s"\nCall stack analysis of anotherFactorial(${input}):\n")
+   println(anotherFactorial(input))
+  // println(anotherFactorial(20000))
 
   // WHEN YOU NEED LOOPS, USE _TAIL_ RECURSION.
 
