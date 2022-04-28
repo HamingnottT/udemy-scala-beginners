d146bdfe79e8eae302652814e615b1f64dce08f7
HamingnottT <Jnottingham836@yahoo.com>
2022-04-28 12:16:08 -0500

    edit recursion practice

--- src/main/scala/exercises/part1basics/recursion1.scala
+++ src/main/scala/exercises/part1basics/recursion1.scala
@@ -17,6 +17,58 @@ object Recursion1 {
         else fibonacci(n - 1) + fibonacci(n - 2)
     }
 
+    def isPrime(n: Int): Boolean = {
+        def isPrimeUntil(t: Int): Boolean = {
+            if (t <= 1) true
+            else n % t != 0 && isPrimeUntil(t-1)
+        }
+        isPrimeUntil(n / 2)
+    }
+
+    def tailrecMenu(): Unit = {
+        var close_menu = false
+        def menuOptions(): Unit = {
+            println(s"""\nThis is a better way of executing exercises problems.
+                        |Please input an the corresponding number to view exercise answers:
+                        |
+                        |1.     Factorial function
+                        |2.     Fibonacci function
+                        |3.     Prime number checker
+                        |99.    Print menu options
+                        |0.    Previous menu
+                        |""".stripMargin)
+        }
+
+        menuOptions()
+
+        while(!close_menu) {
+            var option = readLine("Input option: ").toInt
+
+            if (option == 1) {
+                println("\n")
+                println(factorial(n=readLine("n = ").toInt))
+                println("\n")
+            }
+            else if (option == 2) {
+                println("\n")
+                println(fibonacci(n=readLine("n = ").toInt))
+                println("\n")
+            }
+            else if (option == 3) {
+                println("\n")
+                println(isPrime(n=readLine("n = ").toInt))
+                println("\n")
+            }
+            else if (option == 99) {
+                menuOptions()
+            }
+            else if (option == 0) {
+                println("\nReturning to main menu. . .")
+                return
+            }
+            else println(s"\nInvalid reponse please try again.\n")
+        }
+    }
 
     def menu(): Unit = {
         var close_program = false
@@ -28,6 +80,8 @@ object Recursion1 {
                         |1.     Name greeting
                         |2.     Factorial function
                         |3.     Fibonacci function
+                        |4.     Prime number checker
+                        |5.     Tail recursion
                         |99.    Print menu options
                         |0.     Exit program
                         |""".stripMargin)
@@ -53,16 +107,23 @@ object Recursion1 {
                 println(fibonacci(n=readLine("n = ").toInt))
                 println("\n")
             }
+            else if (option == 4) {
+                println("\n")
+                println(isPrime(n=readLine("n = ").toInt))
+                println("\n")
+            }
+            else if (option == 5) {
+                tailrecMenu()
+            }
             else if (option == 99) {
                 menuOptions()
             }
             else if (option == 0) {
+                println("\nExiting program. . .")
                 close_program = true
             }
             else println(s"\nInvalid reponse please try again.\n")
         }
-
-        println("\nExiting program. . .")
     }
 
     def main(args: Array[String]): Unit = {
