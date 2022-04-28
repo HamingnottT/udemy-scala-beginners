5db21634f15718b487c756c28b9cf0a2137d8da0
HamingnottT <Jnottingham836@yahoo.com>
2022-04-28 16:27:24 -0500

    add recursion practice

--- src/main/scala/exercises/part1basics/recursion1.scala
+++ src/main/scala/exercises/part1basics/recursion1.scala
@@ -1,14 +1,15 @@
 
 import scala.io.StdIn.readLine
+import scala.annotation.tailrec
 
 object Recursion1 {
-
+    
     def greeting(name: String, age: Int): Unit = {
         println(s"Hi, my name is ${name} and I am ${age} years old")
     }
     
     def factorial(n: Int): Int = {
-        if (n < 1) 1
+        if (n <= 1) 1
         else n * factorial(n - 1)
     }
 
@@ -24,6 +25,41 @@ object Recursion1 {
         }
         isPrimeUntil(n / 2)
     }
+
+    def factorialAlt(n: Int, accmulator: Int): Int = {
+        if (n <= 1) accmulator
+        else factorialAlt(n - 1, n * accmulator)
+    }
+
+    def factorialTailrec(n: Int): Int = {
+        @tailrec
+        def factHelper(m: Int, accmulator: Int): Int = {
+            if (m <= 1) accmulator
+            else factHelper(m - 1, m * accmulator)
+        }
+        factHelper(n, 1)
+    }
+
+    def fiboTailrec(n: Int): Int = {
+        def fiboTailrecHelper(m: Int, last: Int, nextToLast: Int): Int = {
+            if (m >= n) last
+            else fiboTailrecHelper(m + 1, last + nextToLast, last)
+        }
+        if (n <= 2) 1
+        else fiboTailrecHelper(2, 1, 1)
+    }
+
+    def isPrimeTailrec(n: Int): Boolean = {
+        @tailrec
+        def isPrimeTailrecHelper(t: Int, isStillPrime: Boolean): Boolean = {
+            if (!isStillPrime) false
+            else if (t <= 1) true
+            else isPrimeTailrecHelper(t - 1, n % t != 0 && isStillPrime)
+        }
+        isPrimeTailrecHelper(n / 2, true)
+    }
+
+
 
     def tailrecMenu(): Unit = {
         var close_menu = false
@@ -46,17 +82,18 @@ object Recursion1 {
 
             if (option == 1) {
                 println("\n")
-                println(factorial(n=readLine("n = ").toInt))
+                println(factorialTailrec(n=readLine("n = ").toInt))
+                // println(factorialAlt(1, 1))
                 println("\n")
             }
             else if (option == 2) {
                 println("\n")
-                println(fibonacci(n=readLine("n = ").toInt))
+                println(fiboTailrec(n=readLine("n = ").toInt))
                 println("\n")
             }
             else if (option == 3) {
                 println("\n")
-                println(isPrime(n=readLine("n = ").toInt))
+                println(isPrimeTailrec(n=readLine("n = ").toInt))
                 println("\n")
             }
             else if (option == 99) {
